aa1b1559992524486ce24c7fdbe4b4281025cdca
Yuki <52448072+Lokki16@users.noreply.github.com>
2022-11-21 20:20:14 +0600

    Update theme_data.dart

--- lib/presentation/template/themes/theme_data.dart
+++ lib/presentation/template/themes/theme_data.dart
@@ -1,7 +1,7 @@
 import 'package:sfera_project_1/presentation/template/template.dart';
 
 class SferaAppTheme {
-  static ThemeData get lightAndroidTheme => ThemeData(
+  static ThemeData get lightTheme => ThemeData(
         brightness: Brightness.light,
         scaffoldBackgroundColor: ThemeColors.white,
       );
